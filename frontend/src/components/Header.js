0b7d57085ca2aefb43c8b1490f3d1f7c27de804f
Lokesh Debnath <lokeshdebnath8@gmail.com>
2023-08-14 22:22:42 +0530

    commit

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,51 @@
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+
+const Header = () => {
+  const { setUserInfo, userInfo } = useContext(UserContext);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, [setUserInfo]);
+
+  function logout() {
+    fetch("http://localhost:4000/logout", {
+      credentials: "include",
+      method: "POST",
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo">
+        MyBlog
+      </Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/post">Create new post</Link>
+            <span onClick={logout}>Logout</span>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/pages/CreatePost.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Editor from "../components/Editor";
+import { Navigate } from "react-router-dom";
+
+const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const createNewPost = async (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("files", files[0]);
+    const response = await fetch("http://localhost:4000/post", {
+      credentials: "include",
+      method: "POST",
+      body: data,
+    });
+
+    if (response.ok) {
+      setRedirect(true);
+    }
+  };
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <form onSubmit={createNewPost}>
+      <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
+        value={summary}
+        onChange={(ev) => setSummary(ev.target.value)}
+      />
+      <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
+      <Editor value={content} onChange={setContent} />
+      <button style={{ marginTop: "5px" }}>Create post</button>
+    </form>
+  );
+};
+
+export default CreatePost;
